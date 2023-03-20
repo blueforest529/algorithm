827d62ef43acf762f14217d6d50f9ab62dd40524
blueforest529 <imcheong001@gmail.com>
2023-03-20 22:17:24 +0900

    baekjoon 11286

---
+++ Week1/11286.py
@@ -0,0 +1,27 @@
+#절댓값 힙
+import heapq
+import sys
+
+n = int(sys.stdin.readline())
+arr = []
+heap = []
+
+for i in range(n):
+    n = int(sys.stdin.readline())
+    arr.append(n)
+
+for i in range(len(arr)):
+    if arr[i] != 0 :
+        res = (abs(arr[i]), arr[i])
+        heapq.heappush(heap, res)
+
+    else : 
+        if heap :
+            print(heapq.heappop(heap)[1])
+        else :
+            print(0)
+
+
+    
+
+# print(heap)
